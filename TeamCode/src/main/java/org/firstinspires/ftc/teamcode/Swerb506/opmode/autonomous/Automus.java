73ae89c59595b8990e8f8bf4e5290fedab0ed43d
MCSpoooof <cachemaola@gmail.com>
2024-07-05 17:02:06 -0400

    Changed the name of Auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/opmode/autonomous/Automus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/opmode/autonomous/Automus.java
@@ -46,7 +46,7 @@ public class Automus extends RobotHardware {
         output = swerveDrive::drive;
 
         ChassisSpeeds currentSpeeds = speedsSupplier.get();
-        path = PathPlannerPath.fromPathFile("Turn Test");
+        path = PathPlannerPath.fromPathFile("Auto");
 
         trajectory = new PathPlannerTrajectory(path, currentSpeeds);
         controller.reset(poseSupplier.get(), currentSpeeds);
