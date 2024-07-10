22aa030a72fea5401c39a523051155359fcb463b
MCSpoooof <cachemaola@gmail.com>
2024-07-10 18:22:26 -0400

    field oriented fixed maybe

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/swerve/SwerveDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/swerve/SwerveDrive.java
@@ -441,9 +441,9 @@ public class SwerveDrive {
      */
     public void zeroGyro() {
         imu.update();
-        imu.setOffset(imu.getRawRotation3d());
+        imu.setOffset(imu.getRawRotation3d().plus(new Rotation3d()));
         swerveController.lastAngleScalar = 0;
-        lastHeadingRadians = 0;
+        lastHeadingRadians = 180;
         resetOdometry(new Pose2d(getPose().getTranslation(), new Rotation2d()));
     }
 
