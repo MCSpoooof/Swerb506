2634cc133b9ca6445f9c549ea5e3132abb4b596d
MCSpoooof <113721215+MCSpoooof@users.noreply.github.com>
2024-08-20 08:35:30 -0400

    Update Automus.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/opmode/autonomous/Automus.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/opmode/autonomous/Automus.java
@@ -21,7 +21,7 @@ import java.util.Map;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
-@Autonomous(name="Pathplanner Test")
+@Autonomous(name="Automus")
 public class Automus extends RobotHardware {
 
     private PathPlannerPath path;
