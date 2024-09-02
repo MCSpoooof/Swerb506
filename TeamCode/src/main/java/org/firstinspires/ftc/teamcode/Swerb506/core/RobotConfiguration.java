54b2c5a1490e81410edd60b6ab54d5e6d11c81a4
MCSpoooof <cachemaola@gmail.com>
2024-09-02 15:12:55 -0400

    pids

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/core/RobotConfiguration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/core/RobotConfiguration.java
@@ -43,105 +43,105 @@ public enum RobotConfiguration {
 
     // Drive motors configuration with PIDF
     DRIVE_FRONT_LEFT(
+            new Motor("cm3")
+                    .configureDirection(DcMotorSimple.Direction.FORWARD)
+                    .configureZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+                    .configureRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+                    .setType(MotorTypes.DRIVE)
+                    .configurePIDF(0.0, 0.0, 0.0, 0.0)
+    ),
+    DRIVE_FRONT_RIGHT(
             new Motor("cm2")
                     .configureDirection(DcMotorSimple.Direction.REVERSE)
                     .configureZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
                     .configureRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
                     .setType(MotorTypes.DRIVE)
-                    .configurePIDF(1.0, 0.0, 0.0, 0.0)
-    ),
-    DRIVE_FRONT_RIGHT(
-            new Motor("cm0")
-                    .configureDirection(DcMotorSimple.Direction.FORWARD)
-                    .configureZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-                    .configureRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-                    .setType(MotorTypes.DRIVE)
-                    .configurePIDF(1.0, 0.0, 0.0, 0.0)
+                    .configurePIDF(0.0, 0.0, 0.0, 0.0)
     ),
     DRIVE_BACK_LEFT(
-            new Motor("cm3")
-                    .configureDirection(DcMotorSimple.Direction.REVERSE)
-                    .configureZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
-                    .configureRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
-                    .setType(MotorTypes.DRIVE)
-                    .configurePIDF(1.0, 0.0, 0.0, 0.0)
-    ),
-    DRIVE_BACK_RIGHT(
             new Motor("cm1")
                     .configureDirection(DcMotorSimple.Direction.FORWARD)
                     .configureZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
                     .configureRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
                     .setType(MotorTypes.DRIVE)
-                    .configurePIDF(1.0, 0.0, 0.0, 0.0)
+                    .configurePIDF(0.0, 0.0, 0.0, 0.0)
+    ),
+    DRIVE_BACK_RIGHT(
+            new Motor("cm0")
+                    .configureDirection(DcMotorSimple.Direction.REVERSE)
+                    .configureZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE)
+                    .configureRunMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER)
+                    .setType(MotorTypes.DRIVE)
+                    .configurePIDF(0.0, 0.0, 0.0, 0.0)
     ),
 
     // Absolute encoders configuration
     ABSOLUTE_FRONT_LEFT(
-            new AbsoluteEncoder("ca1")
-                    .zero(231.27)
+            new AbsoluteEncoder("ca2")
+                    .zero(354.6)
                     .setInverted(false)
     ),
     ABSOLUTE_FRONT_RIGHT(
-            new AbsoluteEncoder("ca2")
-                    .zero(359.23)
+            new AbsoluteEncoder("ca0")
+                    .zero(108.3)
                     .setInverted(false)
     ),
     ABSOLUTE_BACK_LEFT(
-            new AbsoluteEncoder("ca0")
-                    .zero(100.8)
+            new AbsoluteEncoder("ca3")
+                    .zero(80.1)
                     .setInverted(false)
     ),
     ABSOLUTE_BACK_RIGHT(
-            new AbsoluteEncoder("ca3")
-                    .zero(165.7)
+            new AbsoluteEncoder("ca1")
+                    .zero(22.4)
                     .setInverted(false)
     ),
 
     // Continuous servos configuration
     ANGLE_FRONT_LEFT(
-            new ContinuousServo("cs2")
-                    .configureDirection(DcMotorSimple.Direction.FORWARD)
-                    .configurePIDWrapping()
-                    .configurePWMRange(AXON_CONTINUOUS_PWM)
-                    .configurePIDF(0.01, 0.0, 0.0)
-                    .configureFF(0.04)
-                    .configureEncoder(ABSOLUTE_FRONT_LEFT.getAsAbsoluteEncoder())
-    ),
-    ANGLE_FRONT_RIGHT(
-            new ContinuousServo("cs0")
-                    .configureDirection(DcMotorSimple.Direction.FORWARD)
-                    .configurePIDWrapping()
-                    .configurePWMRange(AXON_CONTINUOUS_PWM)
-                    .configurePIDF(0.01, 0.0, 0.0)
-                    .configureFF(0.03)
-                    .configureEncoder(ABSOLUTE_FRONT_RIGHT.getAsAbsoluteEncoder())
-    ),
-    ANGLE_BACK_LEFT(
             new ContinuousServo("cs3")
                     .configureDirection(DcMotorSimple.Direction.FORWARD)
                     .configurePIDWrapping()
                     .configurePWMRange(AXON_CONTINUOUS_PWM)
-                    .configurePIDF(0.01, 0.0, 0.0)
-                    .configureFF(0.04)
-                    .configureEncoder(ABSOLUTE_BACK_LEFT.getAsAbsoluteEncoder())
+                    .configurePIDF(0.0009, 0.0, 0.0)
+                    .configureFF(0.072)
+                    .configureEncoder(ABSOLUTE_FRONT_LEFT.getAsAbsoluteEncoder())
     ),
-    ANGLE_BACK_RIGHT(
+    ANGLE_FRONT_RIGHT(
+            new ContinuousServo("cs2")
+                    .configureDirection(DcMotorSimple.Direction.FORWARD)
+                    .configurePIDWrapping()
+                    .configurePWMRange(AXON_CONTINUOUS_PWM)
+                    .configurePIDF(0.002, 0.0, 0.0)
+                    .configureFF(0.067)
+                    .configureEncoder(ABSOLUTE_FRONT_RIGHT.getAsAbsoluteEncoder())
+    ),
+    ANGLE_BACK_LEFT(
             new ContinuousServo("cs1")
                     .configureDirection(DcMotorSimple.Direction.FORWARD)
                     .configurePIDWrapping()
                     .configurePWMRange(AXON_CONTINUOUS_PWM)
                     .configurePIDF(0.01, 0.0, 0.0)
-                    .configureFF(0.05)
+                    .configureFF(0.07)
+                    .configureEncoder(ABSOLUTE_BACK_LEFT.getAsAbsoluteEncoder())
+    ),
+    ANGLE_BACK_RIGHT(
+            new ContinuousServo("cs0")
+                    .configureDirection(DcMotorSimple.Direction.FORWARD)
+                    .configurePIDWrapping()
+                    .configurePWMRange(AXON_CONTINUOUS_PWM)
+                    .configurePIDF(0.001, 0.0, 0.0)
+                    .configureFF(0.055)
                     .configureEncoder(ABSOLUTE_BACK_RIGHT.getAsAbsoluteEncoder())
     ),
 
     // Encoders for odometry configuration
     ODOMETRY_PARALLEL(
-            new Encoder("cm3")
+            new Encoder("cm0")
                     .setDirection(Encoder.Direction.REVERSE)
     ),
     ODOMETRY_PERPENDICULAR(
-            new Encoder("cm0")
+            new Encoder("cm3")
                     .setDirection(Encoder.Direction.FORWARD)
     );
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/opmode/teleop/SwerveTuning.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Swerb506/opmode/teleop/SwerveTuning.java
@@ -15,22 +15,16 @@ import org.firstinspires.ftc.teamcode.Swerb506.utility.math.geometry.Translation
 
 
 @Config
-@TeleOp(name = "Swerve Tuning")
+@TeleOp(name = "Swerve Tuning", group = "E")
 public class SwerveTuning extends RobotHardware {
     public static double
-            fLOffset = 0, fROffset = 0, bLOffset = 0, bROffset = 0;
+            fLOffset = 0.0, fROffset = 0.0, bLOffset = 0.0, bROffset = 0.0;
 
     public static TuneState state = TuneState.DRIVE;
     public static WheelPosition angleWheel = WheelPosition.FRONT_LEFT;
 
-    // Angle PID parameters
-    public static double aP = 0.08, aI = 0.0, aD = 0.0, aFF = 0.0, angleSetpointStart = 0.0, angleSetpointEnd = 30.0, kStatic = 0.0;
+    public static double aP = 0.08, aI = 0.0, aD = 0.0, aFF = 0.0, angleSetpointStart = 0.0, angleSetpointEnd = 90.0, kStatic = 0.0;
     private double prevAP = aP, prevAI = aI, prevAD = aD, prevAFF = aFF;
-
-    // Drive PID parameters
-    public static double dP = 0.1, dI = 0.0, dD = 0.0, dFF = 0.0;
-    private double prevDP = dP, prevDI = dI, prevDD = dD, prevDFF = dFF;
-
     private boolean reverse = false;
     private ElapsedTimer changeDirection;
     public static double changeDirectionTime = 1.0;
@@ -38,7 +32,7 @@ public class SwerveTuning extends RobotHardware {
     public static boolean precision = false;
     private double precisionMode = 0.35;
 
-    enum WheelPosition {
+    enum WheelPosition{
         FRONT_LEFT,
         FRONT_RIGHT,
         BACK_LEFT,
@@ -54,7 +48,6 @@ public class SwerveTuning extends RobotHardware {
         SINGLE_MOTOR,
         kStatic
     }
-
     private AbsoluteEncoder frontLeft, frontRight, backLeft, backRight;
     private ContinuousServo fL, fR, bL, bR;
 
@@ -71,6 +64,7 @@ public class SwerveTuning extends RobotHardware {
         bL = RobotConfiguration.ANGLE_BACK_LEFT.getAsContinuousServo();
         bR = RobotConfiguration.ANGLE_BACK_RIGHT.getAsContinuousServo();
 
+//        swerveDrive.setMaximumSpeed(1.3);
         changeDirection = new ElapsedTimer();
         changeDirection.reset();
     }
@@ -78,21 +72,19 @@ public class SwerveTuning extends RobotHardware {
     @Override
     public void loop() {
         super.loop();
-        switch (state) {
+        switch(state) {
             case OFFSET:
                 frontLeft.zero(fLOffset);
                 frontRight.zero(fROffset);
                 backLeft.zero(bLOffset);
                 backRight.zero(bROffset);
 
-                for (RobotConfiguration configuration : RobotConfiguration.values()) {
+                for(RobotConfiguration configuration : RobotConfiguration.values()) {
                     HardwareDevice device = configuration.getAsHardwareDevice();
-                    if (device instanceof AbsoluteEncoder) {
+                    if(device instanceof AbsoluteEncoder)
                         telemetry.addData(configuration.name() + " Abs", ((AbsoluteEncoder) device).getCurrentPosition());
-                    }
-                }
-                break;
 
+                }
             case DRIVE:
                 double xVelocity;
                 double yVelocity;
@@ -109,12 +101,63 @@ public class SwerveTuning extends RobotHardware {
                 } else {
                     xVelocity = -primary.left_stick_y * swerveControllerConfiguration.maxSpeed * prec;
                     yVelocity = -primary.left_stick_x * swerveControllerConfiguration.maxSpeed * prec;
-                    angVelocity = -primary.right_stick_x * swerveControllerConfiguration.maxAngularVelocity * prec;
+                    angVelocity = primary.right_stick_x * swerveControllerConfiguration.maxAngularVelocity * prec;
                 }
 
                 swerveDrive.drive(new Translation2d(xVelocity, yVelocity), angVelocity, false, true);
                 break;
+            case ANGLE_PID:
+                if(prevAP != aP || prevAI != aI || prevAD != aD || prevAFF != aFF) {
+                    fL.configurePIDF(aP, aI, aD, aFF);
+                    fR.configurePIDF(aP, aI, aD, aFF);
+                    bL.configurePIDF(aP, aI, aD, aFF);
+                    bR.configurePIDF(aP, aI, aD, aFF);
+                    prevAP = aP;
+                    prevAI = aI;
+                    prevAD = aD;
+                    prevAFF = aFF;
+                    index++;
+                }
 
+
+                double xVelocity$ = -primary.left_stick_y * swerveControllerConfiguration.maxSpeed;
+                double yVelocity$ = -primary.left_stick_x * swerveControllerConfiguration.maxSpeed;
+                double angVelocity$ = primary.right_stick_x * swerveControllerConfiguration.maxAngularVelocity;
+
+                swerveDrive.drive(new Translation2d(xVelocity$, yVelocity$), angVelocity$, false, true);
+                telemetry.addData("Index", index);
+                break;
+            case SINGLE_ANGLE_PID:
+                ContinuousServo servo;
+                switch (angleWheel) {
+                    case FRONT_LEFT:
+                        servo = RobotConfiguration.ANGLE_FRONT_LEFT.getAsContinuousServo();
+                        break;
+                    case FRONT_RIGHT:
+                        servo = RobotConfiguration.ANGLE_FRONT_RIGHT.getAsContinuousServo();
+                        break;
+                    case BACK_LEFT:
+                        servo = RobotConfiguration.ANGLE_BACK_LEFT.getAsContinuousServo();
+                        break;
+                    default:
+                        servo = RobotConfiguration.ANGLE_BACK_RIGHT.getAsContinuousServo();
+                }
+                servo.configurePIDF(aP, aI, aD);
+
+                if(reverse) {
+                    if(changeDirection.seconds() > changeDirectionTime) {
+                        changeDirection.reset();
+                        reverse = false;
+                    }
+                    servo.setReference(angleSetpointStart, aFF);
+                } else {
+                    if(changeDirection.seconds() > changeDirectionTime) {
+                        changeDirection.reset();
+                        reverse = true;
+                    }
+                    servo.setReference(angleSetpointEnd, aFF);
+                }
+                break;
             case SINGLE_MOTOR:
                 Motor motor;
                 switch (angleWheel) {
@@ -131,95 +174,8 @@ public class SwerveTuning extends RobotHardware {
                         motor = RobotConfiguration.DRIVE_BACK_RIGHT.getAsMotor();
                 }
 
-                motor.setPower(-primary.left_stick_y * swerveControllerConfiguration.maxSpeed);
+                motor.setPower(-primary.left_stick_y);
                 break;
-
-            case ANGLE_PID:
-                if (prevAP != aP || prevAI != aI || prevAD != aD || prevAFF != aFF) {
-                    fL.configurePIDF(aP, aI, aD, aFF);
-                    fR.configurePIDF(aP, aI, aD, aFF);
-                    bL.configurePIDF(aP, aI, aD, aFF);
-                    bR.configurePIDF(aP, aI, aD, aFF);
-                    prevAP = aP;
-                    prevAI = aI;
-                    prevAD = aD;
-                    prevAFF = aFF;
-                    index++;
-                }
-
-                double angleXVelocity = -primary.left_stick_y * swerveControllerConfiguration.maxSpeed;
-                double angleYVelocity = -primary.left_stick_x * swerveControllerConfiguration.maxSpeed;
-                double angleAngVelocity = -primary.right_stick_x * swerveControllerConfiguration.maxAngularVelocity;
-
-                swerveDrive.drive(new Translation2d(angleXVelocity, angleYVelocity), angleAngVelocity, false, true);
-                telemetry.addData("Index", index);
-                telemetry.addData("Front Left Servo Position", fL.getPosition());  // Ensure this method exists
-                telemetry.addData("Front Right Servo Position", fR.getPosition());  // Ensure this method exists
-                telemetry.addData("Back Left Servo Position", bL.getPosition());  // Ensure this method exists
-                telemetry.addData("Back Right Servo Position", bR.getPosition());  // Ensure this method exists
-                break;
-
-            case DRIVE_PID:
-                if (prevDP != dP || prevDI != dI || prevDD != dD || prevDFF != dFF) {
-                    RobotConfiguration.DRIVE_FRONT_LEFT.getAsMotor().configurePIDF(dP, dI, dD, dFF);
-                    RobotConfiguration.DRIVE_FRONT_RIGHT.getAsMotor().configurePIDF(dP, dI, dD, dFF);
-                    RobotConfiguration.DRIVE_BACK_LEFT.getAsMotor().configurePIDF(dP, dI, dD, dFF);
-                    RobotConfiguration.DRIVE_BACK_RIGHT.getAsMotor().configurePIDF(dP, dI, dD, dFF);
-                    prevDP = dP;
-                    prevDI = dI;
-                    prevDD = dD;
-                    prevDFF = dFF;
-                    index++;
-                }
-
-                double driveX = -primary.left_stick_y * swerveControllerConfiguration.maxSpeed;
-                double driveY = -primary.left_stick_x * swerveControllerConfiguration.maxSpeed;
-                double driveAng = -primary.right_stick_x * swerveControllerConfiguration.maxAngularVelocity;
-
-                swerveDrive.drive(new Translation2d(driveX, driveY), driveAng, false, true);
-                telemetry.addData("Index", index);
-                telemetry.addData("Front Left Motor Power", RobotConfiguration.DRIVE_FRONT_LEFT.getAsMotor().getPower());
-                telemetry.addData("Front Right Motor Power", RobotConfiguration.DRIVE_FRONT_RIGHT.getAsMotor().getPower());
-                telemetry.addData("Back Left Motor Power", RobotConfiguration.DRIVE_BACK_LEFT.getAsMotor().getPower());
-                telemetry.addData("Back Right Motor Power", RobotConfiguration.DRIVE_BACK_RIGHT.getAsMotor().getPower());
-                telemetry.addData("Front Left Motor Velocity", RobotConfiguration.DRIVE_FRONT_LEFT.getAsMotor().getVelocity());
-                telemetry.addData("Front Right Motor Velocity", RobotConfiguration.DRIVE_FRONT_RIGHT.getAsMotor().getVelocity());
-                telemetry.addData("Back Left Motor Velocity", RobotConfiguration.DRIVE_BACK_LEFT.getAsMotor().getVelocity());
-                telemetry.addData("Back Right Motor Velocity", RobotConfiguration.DRIVE_BACK_RIGHT.getAsMotor().getVelocity());
-                break;
-
-            case SINGLE_ANGLE_PID:
-                ContinuousServo servo;
-                switch (angleWheel) {
-                    case FRONT_LEFT:
-                        servo = RobotConfiguration.ANGLE_FRONT_LEFT.getAsContinuousServo();
-                        break;
-                    case FRONT_RIGHT:
-                        servo = RobotConfiguration.ANGLE_FRONT_RIGHT.getAsContinuousServo();
-                        break;
-                    case BACK_LEFT:
-                        servo = RobotConfiguration.ANGLE_BACK_LEFT.getAsContinuousServo();
-                        break;
-                    default:
-                        servo = RobotConfiguration.ANGLE_BACK_RIGHT.getAsContinuousServo();
-                }
-                servo.configurePIDF(aP, aI, aD, aFF);
-
-                if (reverse) {
-                    if (changeDirection.seconds() > changeDirectionTime) {
-                        changeDirection.reset();
-                        reverse = false;
-                    }
-                    servo.setReference(angleSetpointStart, aFF);
-                } else {
-                    if (changeDirection.seconds() > changeDirectionTime) {
-                        changeDirection.reset();
-                        reverse = true;
-                    }
-                    servo.setReference(angleSetpointEnd, aFF);
-                }
-                break;
-
             case kStatic:
                 ContinuousServo s;
                 switch (angleWheel) {
@@ -236,13 +192,11 @@ public class SwerveTuning extends RobotHardware {
                         s = RobotConfiguration.ANGLE_BACK_RIGHT.getAsContinuousServo();
                 }
                 s.setPower(kStatic);
-                telemetry.addData("Static Power", kStatic);
                 break;
         }
-
-        telemetry.addData("Mode", state);
-        telemetry.addData("Angle Wheel", angleWheel);
-        telemetry.addData("Current Battery Voltage", hardwareMap.voltageSensor.iterator().next().getVoltage()); // Adjust as needed
-        telemetry.update();
+        telemetry.addData("Front Left Position", frontLeft.getCurrentPosition());
+        telemetry.addData("Front Right Position", frontRight.getCurrentPosition());
+        telemetry.addData("Back Left Position", backLeft.getCurrentPosition());
+        telemetry.addData("Back Right Position", backRight.getCurrentPosition());
     }
 }
